3a84e72c1221d3f608155c060c15448479d6166b
Harish <pcdrive23489@gmail.com>
2025-08-17 07:11:28 +0000

    error

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  projectId: "tradevision-journal-d4jwz",
-  appId: "1:566095285056:web:adf1dbb0da03bd6b80c8a5",
-  storageBucket: "tradevision-journal-d4jwz.firebasestorage.app",
-  apiKey: "AIzaSyCl__4klPNRhQBhJYx2eq06uih39NL9KL4",
-  authDomain: "tradevision-journal-d4jwz.firebaseapp.com",
-  measurementId: "",
-  messagingSenderId: "566095285056"
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
 };
 
 
@@ -20,3 +19,4 @@ const auth = getAuth(app);
 const db = getFirestore(app);
 
 export { app, auth, db };
+
